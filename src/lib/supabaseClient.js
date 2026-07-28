dc11ac5d82abae2c18c89e02a92f5d3fdff940ec
emil-prayoga <emilprayogaalbani19@gmail.com>
2026-07-28 16:37:03 +0700

    update supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// Cek di console browser jika environment variable ternyata kosong
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.error("⚠️ SUPABASE KEYS MISSING! Cek Environment Variables di Vercel.");
+}
+
+export const supabase = createClient(supabaseUrl || '', supabaseAnonKey || '');
