51fcc5b649f1b4bf4a7d8b6c2ba8713d9ee37083
emil-prayoga <emilprayogaalbani19@gmail.com>
2026-07-31 01:27:50 +0700

    edit button width 6

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -143,7 +143,7 @@ setStock("");
         </div>
          <button
           onClick={() => setIsOpen(true)}
-          className="w-64 flex items-center gap-2 bg-linear-to-r from-emerald-500 to-teal-500 text-neutral-950 font-semibold px-5 py-2.5 rounded-xl text-sm hover:opacity-90 transition-all disabled:opacity-50 shadow-lg shadow-emerald-950/40"
+          className="w-32 flex items-center gap-2 bg-linear-to-r from-emerald-500 to-teal-500 text-neutral-950 font-semibold px-5 py-2.5 rounded-xl text-sm hover:opacity-90 transition-all disabled:opacity-50 shadow-lg shadow-emerald-950/40"
         >
           Tambah Produk
         </button>
